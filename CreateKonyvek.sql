fafabdd43d4c40426d2d607b278950a03189dee7
Shuu <csaba19999@gmail.com>
2020-12-12 16:57:42 +0100

    Create konyvek megirva

---
+++ CreateKonyvek.sql
@@ -0,0 +1,16 @@
+drop table konyvek;
+create table konyvek
+(
+    konyv_id char(6) not null,
+    raktari_szam char(9) not null,
+    kiado_id int not null,
+    cim varchar2(100) not null,
+    mufaj varchar2(60) not null,
+    kiadas_eve date not null,
+    
+    constraint pk_konyvek primary key(konyv_id),
+    constraint uq_konyvek_raktari_szam unique(raktari_szam),
+    constraint fk_kiadok foreign key(kiado_id)
+        references gyartok(id)
+);
+    

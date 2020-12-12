711a07d497ea9b92a5c663fb23c820b6d339f625
Shuu <csaba19999@gmail.com>
2020-12-12 17:47:03 +0100

    Winform javitva

--- etterem/Properties/AssemblyInfo.cs
+++ etterem/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("beadando")]
+[assembly: AssemblyTitle("etterem")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("beadando")]
+[assembly: AssemblyProduct("etterem")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c45b37e-dc83-49b6-b52f-e39c541ba275")]
+[assembly: Guid("b1e88f04-9eac-46c4-bdd4-049f386843da")]
 
 // Version information for an assembly consists of the following four values:
 //

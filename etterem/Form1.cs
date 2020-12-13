7967dc28aed027c2754c60e773a57264531a40f6
Shuu <csaba19999@gmail.com>
2020-12-13 12:19:32 +0100

    Bugfix modelleknél, winform alap elkezdve

--- etterem/Form1.cs
+++ etterem/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using etterem.Models.Manager;
+using etterem.Models.Records;
 
 namespace etterem
 {
     public partial class Form1 : Form
     {
+
+        KonyvekTabla konyvManager;
+        List<Konyvek> records_KonyvekList;
+        BackgroundWorker bgWorker;
+
         public Form1()
         {
             InitializeComponent();
+            konyvManager = new KonyvekTabla();
+            records_KonyvekList = new List<Konyvek>();
+            bgWorker = new BackgroundWorker();
         }
+
+
+
     }
 }

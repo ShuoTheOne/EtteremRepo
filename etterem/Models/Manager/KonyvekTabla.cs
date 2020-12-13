8ba0c27adefe91ceabe4c6e15795471a7866a702
Shuu <csaba19999@gmail.com>
2020-12-13 12:11:58 +0100

    Record es Manager megirva, bugfixre var

--- etterem/Models/Manager/KonyvekTabla.cs
+++ etterem/Models/Manager/KonyvekTabla.cs
@@ -1,13 +1,281 @@
 ﻿using System;
-using System.Collections.Generic;
+using Oracle.ManagedDataAccess.Client;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using etterem.Models.Records;
 
 namespace etterem.Models.Manager
 {
     class KonyvekTabla
     {
+        OracleConnection GetOracleConnection()
+        {
+            OracleConnection oc = new OracleConnection();
+
+            string connectionString = @"Data Source=193.225.33.71;User Id=ORA_S1340;Password=EKE2020;";
+            oc.ConnectionString = connectionString;
+            return oc;
+        }
+
+        public List<Konyvek> Select()
+        {
+            List<Konyvek> records = new List<Konyvek>();
+
+            OracleConnection oc = new OracleConnection();
+            oc.Open();
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.Text,
+                CommandText = "SELECT ki.nev, ko.mufaj, ko.konyv_id FROM " +
+                " konyvek ko INNER JOIN kiadok ki ON ki.id = ko.kiado_id"
+            };
+
+            command.Connection = oc;
+
+            OracleDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Konyvek konyv = new Konyvek();
+                konyv.Konyv_id = reader["konyv_id"].ToString();
+                konyv.Mufaj = reader["mufaj"].ToString();
+                konyv.Kiado = reader["nev"].ToString();
+
+                records.Add(konyv);
+            }
+            oc.Close();
+
+            return records;
+        }
+
+
+        public int Delete(Konyvek record)
+        {
+            OracleConnection oc = GetOracleConnection();
+            oc.Open();
+
+            OracleTransaction ot = oc.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.Text,
+                CommandText = "DELETE FROM konyvek WHERE konyv_id = :konyv_id"
+            };
+
+            OracleParameter Konyv_idParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = ":konyv_id",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Konyv_id
+            };
+            command.Parameters.Add(Konyv_idParameter);
+
+            command.Connection = oc;
+            command.Transaction = ot;
+
+            int affectedRows = 0;
+            try
+            {
+                affectedRows = command.ExecuteNonQuery();
+                ot.Commit();
+            }
+            catch (Exception)
+            {
+                ot.Rollback();
+            }
+            oc.Close();
+
+            return affectedRows;
+        }
+
+
+        public int Insert(Konyvek record)
+        {
+            OracleConnection oc = GetOracleConnection();
+            oc.Open();
+
+            OracleTransaction ot = oc.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "spInsert_konyvek"
+            };
+
+            OracleParameter Konyv_idParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_konyv_id",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Konyv_id
+            };
+            command.Parameters.Add(Konyv_idParameter);
+
+            OracleParameter Raktari_szamParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_raktari_szam",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Raktari_szam
+            };
+            command.Parameters.Add(Raktari_szamParameter);
+
+            OracleParameter KiadoParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_kiado",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Kiado
+            };
+            command.Parameters.Add(KiadoParameter);
+
+            OracleParameter CimParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_cim",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Cim
+            };
+            command.Parameters.Add(CimParameter);
+
+            OracleParameter MufajParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_mufaj",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Mufaj
+            };
+            command.Parameters.Add(MufajParameter);
+
+            OracleParameter Kiadas_eveParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_kiadas_eve",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = record.Kiadas_eve
+            };
+            command.Parameters.Add(Kiadas_eveParameter);
+
+
+
+            OracleParameter rowcountParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.Int32,
+                ParameterName = "p_out_rowcnt",
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            command.Connection = oc;
+            command.Transaction = ot;
+
+            oc.Close();
+
+            try
+            {
+                command.ExecuteNonQuery();
+                int affectedRows = int.Parse(rowcountParameter.Value.ToString());
+                ot.Commit();
+                return affectedRows;
+            }
+            catch (Exception)
+            {
+                ot.Rollback();
+                return 0;
+            }
+
+
+        }
+
+
+        public bool CheckKonyv_id(string konyv_id)
+        {
+            OracleConnection oc = GetOracleConnection();
+            oc.Open();
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "sf_check_konyv_id"
+            };
+
+            OracleParameter correct = new OracleParameter()
+            {
+                DbType = System.Data.DbType.Int32,
+                Direction = System.Data.ParameterDirection.ReturnValue
+            };
+
+            OracleParameter Konyv_idParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_konyv_id",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = konyv_id
+
+            };
+            command.Parameters.Add(Konyv_idParameter);
+
+            command.Connection = oc;
+
+            try
+            {
+                int successful = int.Parse(correct.Value.ToString());
+
+                return successful != 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
+        }
+
+        public bool CheckRaktari_szam(string raktari_szam)
+        {
+            OracleConnection oc = GetOracleConnection();
+            oc.Open();
+
+            OracleCommand command = new OracleCommand()
+            {
+                CommandType = System.Data.CommandType.StoredProcedure,
+                CommandText = "sf_check_raktari_szam"
+            };
+
+            OracleParameter correct = new OracleParameter()
+            {
+                DbType = System.Data.DbType.Int32,
+                Direction = System.Data.ParameterDirection.ReturnValue
+            };
+
+            OracleParameter Raktari_szamParameter = new OracleParameter()
+            {
+                DbType = System.Data.DbType.String,
+                ParameterName = "p_raktari_szam",
+                Direction = System.Data.ParameterDirection.Input,
+                Value = raktari_szam
+
+            };
+            command.Parameters.Add(Raktari_szamParameter);
+
+            command.Connection = oc;
+
+            try
+            {
+                int successful = int.Parse(correct.Value.ToString());
+
+                return successful != 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+
+        }
+
 
     }
 }
--- etterem/Models/Records/Konyvek.cs
+++ etterem/Models/Records/Konyvek.cs
@@ -1,13 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace etterem.Models.Records
 {
-    class Konyvek
+    public class Konyvek
     {
+        private string konyv_id;
 
+        public string Konyv_id
+        {
+            get
+            {
+                return konyv_id;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("A könyvtári szám nem lehet nulla!");
+                }
+                if (value.Length != 4)
+                {
+                    throw new ArgumentOutOfRangeException("A köyvtári szám 4 karakter hosszú");
+                }
+                konyv_id = value;
+            }
+        }
+
+        private string raktari_szam;
+
+        public string Raktari_szam
+        {
+            get { return raktari_szam; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("A raktári szám nem lehet nulla!");
+                }
+                if (value.Length != 10)
+                {
+                    throw new ArgumentOutOfRangeException("A raktári szám 10 karakter hosszú");
+                }
+                raktari_szam = value;
+            }
+        }
+
+        private string kiado;
+
+        public string Kiado
+        {
+            get
+            {
+                return kiado;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("a kiadó nem lehet null");
+                }
+                kiado = value;
+            }
+        }
+
+        private string cim;
+
+        public string Cim
+        {
+            get
+            {
+                return cim;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("a cím nem lehet null");
+                }
+                cim = value;
+
+            }
+        }
+
+        private string mufaj;
+
+        public string Mufaj
+        {
+            get
+            {
+                return mufaj;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("a műfaj nem lehet null");
+                }
+                mufaj = value;
+
+            }
+        }
+
+        private DateTime kiadas_eve;
+
+        public DateTime Kiadas_eve
+        {
+            get
+            {
+                return kiadas_eve;
+            }
+            set
+            {
+                kiadas_eve = value;
+            }
+        }
     }
 }

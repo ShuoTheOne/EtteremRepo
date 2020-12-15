06037ae5b42df1b6892ac3a0461b6ca6241f38bb
Shuu <csaba19999@gmail.com>
2020-12-15 16:26:59 +0100

    Typok javitva, fájlok belerakva az adatbázisba

--- InsertKonyvek.sql
+++ InsertKonyvek.sql
@@ -1,30 +1,27 @@
 create or replace procedure spInsert_books(
-    p_konyv_id  in books.konyv_id%type,
-    p_raktari_szam in books.raktari_szam%type,
-    p_kiado    in kiadok.nev%type,
-    p_mufaj     in books.mufaj%type,
+	p_konyv_id in books.konyv_id%type,
+	p_raktari_szam in books.raktari_szam%type,
+	p_kiado in kiadok.nev%type,
+    p_mufaj in books.mufaj%type,
     p_cim in books.cim%type,
-    p_kiadas_eve   in books.kiadas_eve%type,
+    p_kiadas_eve in books.kiadas_eve%type,
     p_out_rowcnt out int
-)
+)	
 authid definer
 as
-    v_check_konyv_id int;
+	v_id int;
     v_kiado_id int;
+	v_check_konyv_id int;
 begin
-    p_out_rowcnt := 0;
-    v_kiado_id := sf_getKiadoId(p_kiado);
-    if v_kiado_id is null then
-        spInsert_kiadok(p_kiado);
-        v_kiado_id := sf_getKiadoId(p_kiado);
-    end if;
-    v_check_konyv_id := sf_check_konyv_id(p_konyv_id);
-    if v_check_konyv_id = 1 then
-        insert into books
-            (konyv_id, raktari_szam, kiado_id, mufaj, cim, kiadas_eve)
-        values
-            (p_kiado_id, p_raktari_szam, v_kiado_id, p_mufaj, p_cim, p_kiadas_eve);
-        p_out_rowcnt := SQL%rowcount;
-        commit;
-    end if;
+	select max(konyv_id) into v_id from books;
+		if v_id is null then
+			v_id :=0;
+		end if;
+	v_id := v_id + 1;
+	v_kiado_id := sf_GetKiadoId(p_kiado);
+	v_check_konyv_id := sf_check_konyv_id(p_konyv_id);
+	if v_kiado_id is not null and v_check_konyv_id = 0 then	 
+		insert into books(konyv_id, raktari_szam, kiado_id, mufaj, cim, kiadas_eve)
+			values(v_id, p_raktari_szam, v_kiado_id, p_mufaj, p_cim, p_kiadas_eve);		
+	end if;
 end spInsert_books;

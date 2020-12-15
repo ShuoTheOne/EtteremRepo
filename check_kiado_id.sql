97615293417003b11767dbf479e37468d85335ec
Shuu <csaba19999@gmail.com>
2020-12-15 16:00:51 +0100

    Typok javitva

--- check_kiado_id.sql
+++ check_kiado_id.sql
@@ -3,10 +3,12 @@ create or replace function sf_check_kiado_id
     p_id  in kiadok.id%type
 )
 return int
+deterministic
+as
 begin
     if p_id is null then
         return 0;
     end if;
 
     return 1;
-end sf_check_konyv_id;
+end sf_check_kiado_id;
--- check_konyv_id.sql
+++ check_konyv_id.sql
@@ -4,15 +4,15 @@ create or replace function sf_check_konyv_id
 )
 return int
 deterministic
+as
 begin
     if p_konyv_id is null then
         return 0;
     end if;
-
-    if length(trim(p_konyv_id)) = 4 then
-       	if p_konyv_id >999 and p_konyv_id < 10000 then 
-        	return 1;
-        end if;
+        if length(trim(p_konyv_id)) = 4 then
+            if p_konyv_id >999 and p_konyv_id < 10000 then 
+                return 1;
+            end if;
     end if;
     return 0;
 end sf_check_konyv_id;
